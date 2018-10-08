abcc40c86a3ad40ae8338713aca7a69b9766081c
Jeet Parte <jeetparte@gmail.com>
2018-10-08 20:49:28 +0530

    Add 'Hit Me' Button with alert

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first iOS 12 app!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func knockKnock() {
+        let alert = UIAlertController(title: "Knock knock!", message: "Who's there?", preferredStyle: .actionSheet)
+        
+        let action = UIAlertAction(title: "A new friend", style: .cancel, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
