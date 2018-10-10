63e474b751083b41b150ed7f9e74e160099abe55
Jeet Parte <jeetparte@gmail.com>
2018-10-10 17:11:24 +0530

    Add extra screens

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Jeet Parte on 10/10/18.
+//  Copyright © 2018 Jeet Parte. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Jeet Parte on 10/10/18.
+//  Copyright © 2018 Jeet Parte. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
